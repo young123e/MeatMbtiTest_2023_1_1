90eefe5c79f39e55074fbb133c537bdcc037bf06
young123e <93528188+young123e@users.noreply.github.com>
2024-01-04 17:03:28 +0900

    Add files via upload

---
+++ js/share.js
@@ -0,0 +1,9 @@
+//현재 url 변수로 가져오기
+let nowUrl = window.location.href;
+
+function copyUrl(){ 
+  //nowUrl 변수에 담긴 주소를
+  	navigator.clipboard.writeText(nowUrl).then(res=>{
+	  alert("주소가 복사되었습니다!");
+	})
+}
